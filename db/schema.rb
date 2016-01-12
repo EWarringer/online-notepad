14882b029e86596ebb9e4eb02b06508fe1ebd8f7
EWarringer <erikwarringer@gmail.com>
2016-01-11 20:47:42 -0500

    books migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106222723) do
+ActiveRecord::Schema.define(version: 20160112013622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name",       default: "Untitled", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
