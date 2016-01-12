fe5230dcceed6c6d92d0070d81f3e98ebd531928
EWarringer <erikwarringer@gmail.com>
2016-01-12 01:38:12 -0500

    books model and user unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true
+
+  has_many :books
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user/user_spec.rb
+++ spec/models/user/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   # pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+  it { should have_valid(:username).when('John11', 'Sally11') }
+  it { should_not have_valid(:username).when(nil, '') }
+
+  it { should have_valid(:username).when('John11@gmail.com', 'Sally11@yahoo.com') }
+  it { should_not have_valid(:username).when(nil, '') }
+
+  it { should have_many(:books) }
+end
